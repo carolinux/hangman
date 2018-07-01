5c8f6c6ad4b0ee4cc77d0c4f9a5ef6768cf5aec0
Karolina <karolina.alexiou@siroop.ch>
2018-07-01 22:13:52 +0200

    Write game loop and more tests

--- hangman.py
+++ hangman.py
@@ -1,3 +1,4 @@
+import random
 import unittest
 
 
@@ -8,7 +9,7 @@ class Hangman(object):
     def __init__(self, word):
         self._word = word
         self.tries = self.INITIAL_TRIES
-        self._is_win = True
+        self._is_win = False
         self._shown_text = ['_'] * len(self._word)
 
 
@@ -31,11 +32,13 @@ class Hangman(object):
             for i, c in enumerate(self._word):
                 if char == c:
                     self._shown_text[i] = char
+            if ''.join(self._shown_text) == self._word:
+                self._is_win = True
 
         self.tries-=1
 
     def is_over(self):
-        return self.is_win or self.tries > 0
+        return self.is_win or self.tries <= 0
 
 
 class TestHangman(unittest.TestCase):
@@ -61,6 +64,43 @@ class TestHangman(unittest.TestCase):
         self.assertTrue(game.is_win)
         self.assertTrue(game.is_over)
 
+    def test_win_letter_by_letter(self):
+        game = Hangman('bottle')
+        game.guess_letter('t')
+        game.guess_letter('b')
+        game.guess_letter('o')
+        game.guess_letter('l')
+        game.guess_letter('e')
+        self.assertEquals(game.is_win, True)
+        self.assertEquals(game.tries, 4)
+        self.assertTrue(game.is_win)
+        self.assertTrue(game.is_over)
 
+
+if __name__ == "__main__":
+
+    words = ['fox', 'bank', 'beeswax', 'porcupine', 'bottle', 'carousel']
+    word = random.choice(words)
+    game = Hangman(word)
+    while not game.is_over():
+        print game.shown_text
+        guess = raw_input("Guess a letter or word (tries left: "+str(game.tries)+"): ")
+        if len(guess) == 1:
+            game.guess_letter(guess)
+        else:
+            game.guess_word(guess)
+
+    if game.is_win:
+        print "You won"
+    else:
+        print "You lost"
+        
+
+
+
+
+
+
+    
 
 
